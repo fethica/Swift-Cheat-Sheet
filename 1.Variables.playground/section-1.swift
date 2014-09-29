dc71851d98120bbc6e27db2d3ed3daf92ae858f5
Fethi El Hassasna <e.fethi.c@gmail.com>
2014-09-29 08:40:45 +0100

    Add variables playground

---
+++ 1.Variables.playground/section-1.swift
@@ -0,0 +1,22 @@
+// Playground - noun: a place where people can play
+//Created by: Fethi El Hassasna @fethica
+
+import UIKit
+
+//Type interference
+
+var myIntNumber = 100
+var myStringName = "Fethi"
+var myBoolValue = true
+
+//Basic data types
+
+var myInt : Int
+
+var myFloat : Float
+var myDouble : Double
+
+var myString : String
+var myCharacter : Character
+
+var myBool : Bool
