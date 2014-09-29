08a506b44a5ee75c7ee4f75370f2261449ec1235
Fethi El Hassasna <e.fethi.c@gmail.com>
2014-09-29 11:52:02 +0100

    Add constants playground

---
+++ 2.Constants.playground/section-1.swift
@@ -0,0 +1,12 @@
+// Playground - noun: a place where people can play
+// Created by: Fethi El Hassasna @fethica
+
+import UIKit
+
+let myString1 = "Hello, playground"
+let myString2 : String = "Hello, playground"
+
+// "var" represents mutability in Objective-C
+
+let str1 = "Hello, playground" // NSString *str1 = @"Hello, playground";
+var str2 = "Hello, playground" // NSMutableString *str2 = @"Hello, playground";
