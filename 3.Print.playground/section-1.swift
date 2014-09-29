7a793521ce3300cebc8e2113b69b90c219ea8ccb
Fethi El Hassasna <e.fethi.c@gmail.com>
2014-09-29 11:58:53 +0100

    Add print playground

---
+++ 3.Print.playground/section-1.swift
@@ -0,0 +1,19 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+print("Hello, playground")
+
+let x = 49
+let y = 33
+
+print("x * y = \(x * y)")
+
+let width = 450
+let height = 550.85
+
+// To convert the result use: Int() Double() String() ...
+
+print("Surface = \(Double(width) * height)")
+
+println("Surface = \(width * Int(height))")
