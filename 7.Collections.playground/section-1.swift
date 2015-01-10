5bc6a9cfbabfc7fc493922c12ea2be511e5320fd
Fethi El Hassasna <e.fethi.c@gmail.com>
2015-01-10 12:12:49 +0100

    Add Dictionaries to the collections playground

--- 7.Collections.playground/section-1.swift
+++ 7.Collections.playground/section-1.swift
@@ -33,3 +33,34 @@ if !array.isEmpty {
 for item in array {
     println(item)
 }
+
+// Dictionaries
+
+//NSDictionary
+let countries1 = [ "DZ": "Algeria", "US": "United States", "UK": "United Kingdom", "JP": "Japan"]
+
+//NSMutableDictionary
+var countries2 = [ "DZ": "Algeria", "US": "United States", "UK": "United Kingdom", "JP": "Japan"]
+
+//Declare a dictionary without an initial value
+var dic: [Int:String]
+
+//Accessing value
+println(countries2["DZ"])
+
+// Insert OR Change
+countries2["GR"] = "Germany"
+
+countries2["US"] = "USA"
+
+countries2.updateValue("France", forKey: "FR")
+
+// Delete value
+countries2["UK"] = nil
+countries2.removeValueForKey("GR")
+
+println("There are \(countries2.count) in the dictionary")
+
+for (key, value) in countries1 {
+    println("Key: \(key), value: \(value)")
+}
